0d401f54d176048f6d0a337831cfd7ce3d6edde9
srkimdev <monbgqvjk@naver.com>
2024-02-22 18:25:36 +0900

    연결요소의 개수

---
+++ 그래프이론/11724_연결 요소의 개수.py
@@ -0,0 +1,28 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(2500)
+
+n, m = map(int, input().split())
+
+visited = [False] * (n + 1)
+
+graph = [[] for _ in range(n + 1)]
+for i in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+count = 0
+
+def dfs(graph, v, visited):
+    visited[v] = True
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph, i, visited)
+
+for i in range(1, n + 1):
+    if not visited[i]:
+        dfs(graph, i, visited)
+        count += 1
+
+print(count)
