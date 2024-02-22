4f22352894d279f88fd67a95d6c5722a5b0c325e
srkimdev <monbgqvjk@naver.com>
2024-02-22 18:02:32 +0900

    1, 2, 3 더하기

---
+++ 다이나믹 프로그래밍/9095_1, 2, 3 더하기.py
@@ -0,0 +1,14 @@
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    
+    d = [0] * 12
+    d[1] = 1
+    d[2] = 2
+    d[3] = 4
+
+    for i in range(4, n + 1):
+        d[i] =  d[i - 3] + d[i - 2] + d[i - 1]
+
+    print(d[n])
