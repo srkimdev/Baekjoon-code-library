8a3336299087b71c8986de108998bf55a2739c23
srkimdev <monbgqvjk@naver.com>
2024-02-27 16:44:33 +0900

    흙길 보수하기

---
+++ 그리디/1911_흙길 보수하기.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+n, l = map(int, input().split())
+hole = [list(map(int, input().split())) for _ in range(n)]
+
+hole.sort()
+boundary = hole[0][0]
+
+cur, count = 0, 0
+for start, end in hole:
+    if start > end:
+        continue
+    if cur > start:
+        start = cur
+    while start < end:
+        start += l
+        count += 1
+    cur = start
+
+print(count)
