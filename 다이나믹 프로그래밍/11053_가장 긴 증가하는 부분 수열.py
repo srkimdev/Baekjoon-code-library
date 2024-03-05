2821ec5e65f04596184324815fc1951a2230b628
srkimdev <monbgqvjk@naver.com>
2024-03-05 14:42:27 +0900

    가장 긴 증가하는 부분 수열

---
+++ 다이나믹 프로그래밍/11053_가장 긴 증가하는 부분 수열.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+array = list(map(int, input().split()))
+dp = [1] * n
+
+mx = 0
+for i in range(1, n):
+    for j in range(i):
+        if array[i] > array[j]:
+            dp[i] = max(dp[i], dp[j] + 1)
+
+print(max(dp))
