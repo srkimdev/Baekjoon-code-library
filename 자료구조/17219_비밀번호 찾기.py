04ff12e11b49d0d20e95613d95ecdb4bc43f8a57
srkimdev <monbgqvjk@naver.com>
2024-03-13 21:25:08 +0900

    비밀번호 찾기

---
+++ 자료구조/17219_비밀번호 찾기.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+dic = {}
+for _ in range(n):
+    a, b = input().split()
+    dic[a] = b
+
+for _ in range(m):
+    s = input().strip()
+    print(dic[s])
