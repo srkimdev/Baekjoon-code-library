7da30c1351bb974ba753421ab9d4451173fce1da
srkimdev <emirated3858@gmail.com>
2024-03-22 21:22:09 +0900

    공유기 설치

---
+++ 이진탐색/2110_공유기 설치.py
@@ -0,0 +1,34 @@
+import sys
+input = sys.stdin.readline
+
+n, c = map(int, input().split())
+
+home = []
+for _ in range(n):
+    home.append(int(input()))
+
+home.sort()
+
+start = 1
+end = home[-1] - home[0]
+result = 0
+
+while start <= end:
+    mid = (start + end) // 2
+    current = home[0]
+    count = 1
+    
+    for i in range(1, len(home)):
+        if home[i] >= current + mid:
+            count += 1
+            current = home[i]
+    
+    if count >= c:
+        start = mid + 1
+        result = mid
+    else:
+        end = mid - 1
+
+print(result)
+
+
